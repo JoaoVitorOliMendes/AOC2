36ae8e18ff4a0b5b0652e028bfc979ca5b8eed3b
JoaoVitorOliMendes <joaovitordeoliveiramendes@tutanota.com>
2023-11-19 20:53:39 -0300

    RISC

--- main.js
+++ main.js
@@ -2,9 +2,11 @@ tableCmd = document.getElementById('tableCmd');
 cmdForm = document.getElementById('cmdForm');
 txtCommands = document.getElementById('commands');
 txtIndex = document.getElementById('index');
+txtConsole = document.getElementById('console');
 btnExecute = document.getElementById('btnExecute');
+btnSubmit = document.getElementById('btnSubmit');
 
-registerBank = [document.getElementById('reg0'), document.getElementById('reg1'), document.getElementById('reg2'), document.getElementById('reg3')]
+registerBank = [document.getElementById('reg0'), document.getElementById('reg1'), document.getElementById('reg2'), document.getElementById('reg3'), document.getElementById('reg4'), document.getElementById('reg5')]
 
 function deleteClick(e) {
     parentChild = e.target.parentNode.parentNode.children
@@ -67,9 +69,11 @@ function executeCommand(command) {
             sgt(params.splice(0,2), params[params.length-1])
         }else if (!opcode1[0] && opcode1[1] && opcode1[2] && !opcode1[3]) {
             console.log('bne');
+            console.log(params);
             bne(params.splice(0,2), params[params.length-1])
         }
     }
+    consoleAppend('')
     return 1
 }
 
@@ -106,10 +110,12 @@ function getVals(command, addressQty) {
     for (let i=0;i<addressQty;i++) {
         isReg = command.substr(-4*(i+1),1) == true
         bits = command.substr(-4*(i+1),4).substring(1)
+        console.log(registerBank.length-1, i)
         if (isReg) {
             registers.unshift(registerBank[parseInt(bits, 2)])
-        }else {
-            registers.unshift(parseInt(bits, 2))
+        } else {
+            mov(parseInt(bits, 2), registerBank[registerBank.length - 1 - i])
+            registers.unshift(registerBank[registerBank.length - 1 - i])
         }
     }
     return registers
@@ -129,43 +135,57 @@ function jmp(val) {
     txtIndex.value=getValValue(val) - 1
     txtIndex.dispatchEvent(new Event('input'))
     // btnExecute.dispatchEvent(new Event('click'))
+    consoleAppend("jmp " + val.id)
 }
 
 //1111 0000 XXXX XXXX
 function mov(val, destiny) {
-    destiny.value = getValValue(val)
+    console.log(val, destiny);
+    valValue = getValValue(val)
+    destiny.value = valValue
+    consoleAppend("mov " + destiny.id + " " + valValue)
 }
 
 //0000 XXXX XXXX XXXX
 function add(vals, destiny) {
     destiny.value = ula(getValValue(vals[0]), getValValue(vals[1]), 1)
+    consoleAppend("add " + vals[0].id + " " + vals[1].id + " " + destiny.id)
 }
 //0001 XXXX XXXX XXXX
 function sub(vals, destiny) {
     destiny.value = ula(getValValue(vals[0]), getValValue(vals[1]), 2)
+    consoleAppend("sub " + vals[0].id + " " + vals[1].id + " " + destiny.id)
 }
 //0010 XXXX XXXX XXXX
 function mlt(vals, destiny) {
     destiny.value = ula(getValValue(vals[0]), getValValue(vals[1]), 3)
+    consoleAppend("mlt " + vals[0].id + " " + vals[1].id + " " + destiny.id)
 }
 //0011 XXXX XXXX XXXX
 function div(vals, destiny) {
     destiny.value = ula(getValValue(vals[0]), getValValue(vals[1]), 4)
+    consoleAppend("div " + vals[0].id + " " + vals[1].id + " " + destiny.id)
 }
 //0100 XXXX XXXX XXXX
 function slt(vals, destiny) {
     destiny.value = ula(getValValue(vals[0]), getValValue(vals[1]), 5) ? 1 : 0
+    consoleAppend("slt " + vals[0].id + " " + vals[1].id + " " + destiny.id)
 }
 //0101 XXXX XXXX XXXX
 function sgt(vals, destiny) {
     destiny.value = ula(getValValue(vals[0]), getValValue(vals[1]), 6) ? 1 : 0
+    consoleAppend("sgt " + vals[0].id + " " + vals[1].id + " " + destiny.id)
 }
 //0110 XXXX XXXX XXXX
 function bne(vals, destiny) {
-    console.log(ula(getValValue(vals[0]), getValValue(vals[1]), 7));
     if (ula(getValValue(vals[0]), getValValue(vals[1]), 7)) {
         jmp(destiny)
     }
+    consoleAppend("bne " + vals[0].id + " " + vals[1].id + " " + destiny.id)
+}
+
+function consoleAppend(message) {
+    txtConsole.value += message + '\n'
 }
 
 cmdForm.addEventListener('submit', (e) => {
@@ -217,6 +237,7 @@ for (let i=0;i<32;i++) {
     `)
 }
 
+txtConsole.value=""
 txtIndex.value=0
 txtIndex.dispatchEvent(new Event('input'))
 
@@ -224,7 +245,19 @@ for (i of registerBank) {
     i.value='∅'
 }
 
+startCmds = [
+    "1111000000001000",
+    "0000000110001000",
+    "0100010110001001",
+    "0110000010010101",
+    "1111111000000001",
+    "1111000001111011"
+]
 
+for (let i = 0; i < startCmds.length; i++) {
+    cmdArray = document.getElementsByName('cmd')
+    cmdArray[i].value = startCmds[i]
+}
 /*
 SIMPLE FOR
 1111000000001000
